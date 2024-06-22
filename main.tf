bcab70d272abdd5c964259545dbd5c7395d46696
Joseph Guaneri <joe@guaneri.com>
2024-06-21 20:37:42 -0400

    Forgot to declare attribute

--- main.tf
+++ main.tf
@@ -43,6 +43,6 @@ data aws_route_table "rts" {
 resource aws_vpc_endpoint_route_table_association "rta" {
   count = length(data.aws_route_table.rts)
 
-  route_table_id = data.aws_route_table.rts[count.index].id
+  route_table_id = tolist(data.aws_route_table.rts.id)[count.index]
   vpc_endpoint_id = aws_vpc_endpoint.vpce.id
 }
