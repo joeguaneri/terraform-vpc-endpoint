fe58ee8bdf9882cd090141c596e818d67af6a022
Joseph Guaneri <joe@guaneri.com>
2024-06-21 20:19:48 -0400

    Adding in gateway route associations

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ variable "endpoint_type" {
   default = "Interface"
 }
 
-resource "aws_vpc_endpoint" "vpce" {
+resource aws_vpc_endpoint "vpce" {
   vpc_id              = var.vpc_id
   service_name        = var.service_name
   vpc_endpoint_type   = var.endpoint_type
@@ -27,9 +27,21 @@ resource "aws_vpc_endpoint" "vpce" {
   security_group_ids  = var.endpoint_type == "Interface" ? var.security_group_ids : null
 }
 
-resource "aws_vpc_endpoint_subnet_association" "sna" {
+resource aws_vpc_endpoint_subnet_association "sna" {
   for_each = var.endpoint_type == "Interface" ? var.subnet_ids : []
 
   vpc_endpoint_id = aws_vpc_endpoint.vpce.id
   subnet_id       = each.value
+}
+
+data aws_route_table "rt" {
+  for_each = var.endpoint_type != "Interface" ? var.subnet_ids : []
+  subnet_id = each.value
+}
+
+resource aws_vpc_endpoint_route_table_association "rta" {
+  for_each = var.endpoint_type != "Interface" ? data.aws_route_table.rt : []
+
+  route_table_id = each.value
+  vpc_endpoint_id = aws_vpc_endpoint.vpce.id
 }
